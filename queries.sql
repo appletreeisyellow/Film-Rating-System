8888c0185aec7524bbb309c3438a2dbaa9f7b4be
whatahappyday <yccvivo@gmail.com>
2017-04-12 15:00:08 -0700

    Start on queries.sql

---
+++ queries.sql
@@ -0,0 +1,18 @@
+
+# Give me the names of all the actors in the movie 'Die Another Day'. 
+# Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by a single space). 
+# You may need to use MySQL CONCAT Function (very important).
+
+
+
+# Give me the count of all the actors who acted in multiple movies.
+
+
+
+# Give me the title of movies that sell more than 1,000,000 tickets.
+
+
+# You name it 1
+
+
+# You name it 2
